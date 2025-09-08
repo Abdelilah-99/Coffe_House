d31f9453f75ca4a371d2212cb47c0f20a2bcab4d
babdelil <bouchikhiabdelilah0@gmail.com>
2025-09-08 16:57:28 +0100

    fix the name of table to be unmatched with postgres names

--- backend/src/main/java/com/blog/entity/Like.java
+++ backend/src/main/java/com/blog/entity/Like.java
@@ -5,9 +5,11 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import jakarta.persistence.Entity;
 
 @Entity
+@Table(name = "likes")
 public class Like {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- backend/src/main/java/com/blog/entity/User.java
+++ backend/src/main/java/com/blog/entity/User.java
@@ -4,6 +4,7 @@ import java.util.*;
 import jakarta.persistence.*;
 
 @Entity
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
