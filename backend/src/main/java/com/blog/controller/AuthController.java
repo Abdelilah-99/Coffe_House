f74111ce861d6c5fd0063437d2ddc55eb278a423
babdelil <bouchikhiabdelilah0@gmail.com>
2025-09-12 00:13:23 +0100

    forget to protect the passeord fixing + moving the jwt creation logic to the service layer

--- backend/src/main/java/com/blog/controller/AuthController.java
+++ backend/src/main/java/com/blog/controller/AuthController.java
@@ -2,46 +2,35 @@ package com.blog.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
-import com.blog.config.JwtUtils;
 import com.blog.dto.AuthRequest;
 import com.blog.dto.AuthResponse;
 import com.blog.dto.RegisterRequest;
 import com.blog.dto.RegisterResponse;
 import com.blog.exceptions.InvalidPasswordException;
 import com.blog.exceptions.UserNotFoundException;
-import com.blog.service.CustomUserDetailsService;
+import com.blog.service.AuthService;
 import com.blog.service.RegistrationService;
 import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
-    private final CustomUserDetailsService userDetailsService;
-    private final JwtUtils jwtUtils;
     private final RegistrationService registerService;
+    private final AuthService authService;
 
     public AuthController(
-            CustomUserDetailsService userDetailsService,
-            JwtUtils jwtUtils,
+            AuthService authService,
             RegistrationService registerService) {
-        this.userDetailsService = userDetailsService;
-        this.jwtUtils = jwtUtils;
         this.registerService = registerService;
+        this.authService = authService;
     }
 
     @PostMapping("/login")
     public ResponseEntity<AuthResponse> login(@RequestBody AuthRequest req) {
         try {
-            UserDetails userDetails = userDetailsService.loadUserByUsername(req.getUsername());
-            String token = jwtUtils.generateToken(userDetails);
-            AuthResponse res = new AuthResponse(
-                    "Login successful",
-                    userDetails.getAuthorities().toString(),
-                    userDetails.getUsername(),
-                    token);
+            AuthResponse res = authService.login(req);
             return ResponseEntity.ok(res);
         } catch (UserNotFoundException e) {
             return ResponseEntity.status(404).build();
